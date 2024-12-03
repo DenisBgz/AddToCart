f4f170b432c5d6c7e79079ddef36fe3c686a7a49
Xore <bogomazov2007@yandex.ru>
2024-12-03 21:34:07 +0300

    fix:added functional for addcart button

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
+function addElement(symbol,type,cls){
+	let buttonSection = document.getElementById('buttonContainer')
+	let plusButton = document.createElement(type);
+	plusButton.className = cls;
+	plusButton.innerHTML = symbol;
+	buttonSection.appendChild(plusButton);
+}
+function removeElement(id){
+	let element = document.getElementsByClassName(id);
+	element[0].parentNode.removeChild(element[0]);
+}
 let imageArray = document.getElementsByClassName('icon-img');
 let imageBoxArray = document.getElementsByClassName('image-box');
-const titleImage = document.getElementById('title-img')
-
+let addToBagButton = document.getElementById('AddToCart');
+const titleImage = document.getElementById('title-img');
+let added = false;
+imageBoxArray[0].style.border = "0.25vh solid rgb(67, 201, 172)";
 for(let i = 0;i<imageArray.length;i++){
 	imageBoxArray[i].onclick = ()=>{
 		for(let j = 0;j < imageArray.length;j++)
@@ -10,5 +23,25 @@ for(let i = 0;i<imageArray.length;i++){
 		titleImage.src = imageArray[i].src;
 		imageBoxArray[i].style.border = "0.25vh solid rgb(67, 201, 172)";
 	}
-
+}
+addToBagButton.onclick = ()=>{
+	if(!added){
+		addToBagButton.style.width = "55%";
+		addToBagButton.textContent = "Добавлено в корзину";
+		addToBagButton.style.backgroundColor = "rgb(184, 255, 237)";
+		addToBagButton.style.color = "rgb(67, 201, 172)"
+		addElement('-','button','minus-button')
+		addElement('1','p','quantity')
+		addElement('+','button','plus-button')
+		added = true;
+	}else{
+		removeElement('plus-button');
+		removeElement('minus-button');
+		removeElement('quantity');
+		addToBagButton.style.width = "100%"
+		addToBagButton.style.color = "white"
+		addToBagButton.style.backgroundColor = "rgb(67, 201, 172)"
+		addToBagButton.textContent = "Добавить в корзину";
+		added = false;
+	}
 }
