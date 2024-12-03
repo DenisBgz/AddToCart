4c03dcca250961a3700e6492494daf92ebdd32df
Xore <bogomazov2007@yandex.ru>
2024-12-03 14:53:37 +0300

    feat: Added functional of picture selection

--- js/app.js
+++ js/app.js
@@ -1,2 +1,14 @@
+let imageArray = document.getElementsByClassName('icon-img');
+let imageBoxArray = document.getElementsByClassName('image-box');
+const titleImage = document.getElementById('title-img')
 
- 
+for(let i = 0;i<imageArray.length;i++){
+	imageBoxArray[i].onclick = ()=>{
+		for(let j = 0;j < imageArray.length;j++)
+			imageBoxArray[j].style.border = "none";
+		
+		titleImage.src = imageArray[i].src;
+		imageBoxArray[i].style.border = "0.25vh solid rgb(67, 201, 172)";
+	}
+
+}
